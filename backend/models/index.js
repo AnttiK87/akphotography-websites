56d1a916af01b2bd0554764a5f8db266eb9cb4e3
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-03-14 10:05:15 +0200

    building commenting feature both backend and frontend. Also other tweaks for front end. Worked for this about 20 h

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,19 +2,20 @@
 const Picture = require('./picture')
 const Text = require('./text')
 const Rating = require('./rating')
+const Comment = require('./comment')
 
-// Picture hasOne Text
 Picture.hasOne(Text, { foreignKey: 'pictureId', onDelete: 'CASCADE' })
-// Text belongsTo Picture
 Text.belongsTo(Picture, { foreignKey: 'pictureId' })
 
-// Picture hasOne Text
 Picture.hasMany(Rating, { foreignKey: 'pictureId', onDelete: 'CASCADE' })
-// Text belongsTo Picture
 Rating.belongsTo(Picture, { foreignKey: 'pictureId' })
+
+Picture.hasMany(Comment, { foreignKey: 'pictureId', onDelete: 'CASCADE' })
+Comment.belongsTo(Picture, { foreignKey: 'pictureId' })
 
 module.exports = {
   Picture,
   Text,
   Rating,
+  Comment,
 }
