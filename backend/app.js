50ee5615600f9028a30a8b322b8f426402adc671
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-04-03 10:48:42 +0300

    setting up deployment pipeline

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,12 @@ app.use('/api/logout', logoutRouter)
 
 connectToDatabase()
 
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
