fb526ef0da8754b74587f866bc3661b91107a826
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-07-04 14:53:00 +0300

    new tests etc.

--- backend/src/utils/multerConfig.ts
+++ backend/src/utils/multerConfig.ts
@@ -1,44 +1,26 @@
 // utils/multerConfig.ts
 import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
 
+import { AppError } from '../errors/AppError.js';
 import logger from './logger.js';
 
-// path for highResFiles
-const isTestEnv = process.env.NODE_ENV === 'test';
-const uploadBase = isTestEnv ? './tests/uploads/' : './uploads/';
-const uploadFolderHightRes = path.join(uploadBase, 'pictures');
-const uploadFolderThumbnail = path.join(uploadBase, 'thumbnail');
+const storage = multer.memoryStorage();
 
-// if the folders do not exist, create them
-[uploadFolderHightRes, uploadFolderThumbnail].forEach((folder) => {
-  if (!fs.existsSync(folder)) {
-    fs.mkdirSync(folder, { recursive: true });
-  }
-});
-
-// configure multer
-const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => {
-    cb(null, uploadFolderHightRes);
-  },
-  filename: (_req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
+// configure multer for upload file to memory with file type and size limit
 const upload = multer({
   storage,
+  limits: {
+    fileSize: 6 * 1024 * 1024, // 6 MB limit
+  },
   fileFilter: (_req, file, cb) => {
     const allowed = ['image/jpeg'];
     if (allowed.includes(file.mimetype)) {
       logger.info('uploading file:', file.originalname);
       cb(null, true);
     } else {
-      cb(new Error('Only .jpg files are allowed!'));
+      throw new AppError({ en: 'Only .jpg files are allowed!' }, 401);
     }
   },
 });
 
-export { upload, uploadFolderHightRes, uploadFolderThumbnail };
+export { upload };
