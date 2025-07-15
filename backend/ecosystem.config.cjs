98fc788162900b6afc24892c3f80d2e0caddb061
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-07-15 15:17:19 +0300

    Changes to ecosystem.config

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'AK-photography-websites-backend',
-      script: './index.js',
+      script: '/home2/chgqkclj/public_html/index.js',
       instances: 1,
       autorestart: true,
       watch: false,
