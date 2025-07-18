22a16fbdc5b2bdc53e5d642ac1bec24acff27d82
Antti Kortelainen <Antti.Kortelainen@edu.lapinamk.fi>
2025-07-18 09:24:29 +0300

    updated dependencies and changed workflow. This deploy should fail and rollback to previous tag

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
+
+const categories = ["mammals", "landscapes", "nature", "birds"];
+const galleryRoutes = categories.flatMap((c) => [`/pictures/${c}`]);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://www.akphotography.fi",
+      routes: ["/", "/info", "/contact", ...galleryRoutes],
+      // valinnainen:
+      outDir: "dist", // minne sitemap kirjoitetaan
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
